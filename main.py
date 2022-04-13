a627a126d99314fe9046ab51a4b29ca5c6e68c0c
187-Shogun <187shogun@gmail.com>
2022-04-12 18:46:41 -0600

    Modefied script.

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ Description: <Some description>
 """
 
 
-from sklearn.preprocessing import MinMaxScaler
 from matplotlib import pyplot as plt
+from datetime import datetime
+from pytz import timezone
 import tensorflow as tf
 import pandas as pd
 import numpy as np
@@ -20,82 +21,80 @@ import os
 
 
 # Global vars:
-EPOCHS = 100
+EPOCHS = 25
 PATIENCE = 12
 RANDOM_SEED = 420
 SEQ_LEN = 36
 BATCH_SIZE = 32
-TARGETS = 1
+TARGETS = 10
 FEATURES = 2
 LOGS_DIR = os.path.join(os.getcwd(), 'logs')
 MODELS_DIR = os.path.join(os.getcwd(), 'models')
 AUTOTUNE = tf.data.AUTOTUNE
 
 
-def get_scaler() -> MinMaxScaler:
-    """ Get scaler for transforming data. """
-    df = fetch_raw_data(TARGETS)
-    scaler = MinMaxScaler()
-    scaler.fit(df.T_out.values.reshape(-1, 1))
-    return scaler
+def get_model_version_name(model_name: str) -> str:
+    """ Generate a unique name using timestamps. """
+    ts = datetime.now(timezone('America/Costa_Rica'))
+    run_id = ts.strftime("%Y%m%d-%H%M%S")
+    return f"{model_name}_v.{run_id}"
 
 
-def fetch_raw_data(targets: int) -> pd.DataFrame:
+def fetch_raw_data() -> pd.DataFrame:
     """ Read raw data into a pandas df. """
     # Read raw data and preprocess it:
     df = pd.read_csv(r"data/energy_data.csv")
     df.date = pd.to_datetime(df.date)
-    df = df[['date', 'Appliances', 'T_out']].sort_values('date')
+    df = df.sort_values('date')
     df['ampm'] = df.date.apply(lambda x: 0 if x.hour in range(6, 18) else 1).astype(float)
     df['doy'] = df.date.apply(lambda x: x.strftime('%j')).astype(int)
-    df.Appliances = df.Appliances.shift(-targets)
-    df = df.dropna()
     df = df.join(pd.get_dummies(df.doy).astype(float))
-    df = df.drop(columns=['date', 'doy'])
+    df = df.drop(columns=['doy', 'lights'])
     return df
 
 
-def fetch_preprocessed_data(df: pd.DataFrame) -> pd.DataFrame:
-    """ Get pandas df after applying preprocessing steps. """
-    scaler = get_scaler()
-    df.T_out = scaler.transform(df.T_out.values.reshape(-1, 1)).reshape(-1)
-    return df
-
-
-def fetch_dataset(seq_size: int, batch_size: int, targets: int, features_count: int, train_split: float) -> tuple:
-    """ Cast pandas dataframe into TF Datasets. """
+def fetch_dataset(train_split: float = 0.8) -> tuple:
+    """ Cast pandas dataframe into TF Datasets. Returns Test, Validation and Test Datasets. """
     # Fetch raw data:
-    df = fetch_raw_data(targets)
-    df = fetch_preprocessed_data(df)
-    labels = df.columns[:1]
-    features = df.columns[1:]
+    df = fetch_raw_data()
+    X = df.iloc[:, 1:FEATURES + 1].values
+    y = df[['Appliances']].values
+    features_list = []
+    labels_list = []
+
+    for i in range(df.shape[0] - SEQ_LEN - TARGETS):
+        features = X[i: i + SEQ_LEN + TARGETS]
+        labels = y[i: i + SEQ_LEN + TARGETS]
+        features_list.append(features[:SEQ_LEN])
+        labels_list.append(labels[SEQ_LEN:])
 
     # Configure a TF dataset:
-    X_dataset = tf.data.Dataset.from_tensor_slices(df[features[:features_count]].values)
-    y_dataset = tf.data.Dataset.from_tensor_slices(df[labels[0]].values)
-    X_windowed = X_dataset.window(size=seq_size, shift=1, drop_remainder=True)
-    y_windowed = y_dataset.window(size=seq_size, shift=1, drop_remainder=True)
-    X_batched = X_windowed.flat_map(lambda x: x.batch(seq_size))
-    y_batched = y_windowed.flat_map(lambda x: x.batch(seq_size))
-    dataset = tf.data.Dataset.zip((X_batched, y_batched))
-    dataset = dataset.map(lambda x, y: (x, y[-1])).shuffle(10_000).batch(batch_size).prefetch(AUTOTUNE)
+    X_ds = tf.data.Dataset.from_tensor_slices(features_list)
+    y_ds = tf.data.Dataset.from_tensor_slices(labels_list)
+    dataset = tf.data.Dataset.zip((X_ds, y_ds)).batch(BATCH_SIZE)
+    dataset = dataset.shuffle(10_000).prefetch(AUTOTUNE)
 
     # Return split datasets:
-    total_records = len(y_windowed)
-    training_size = int(total_records * train_split) // batch_size
-    return dataset.take(training_size), dataset.skip(training_size)
+    total_records = len(dataset)
+    training_size = int(total_records * train_split)
+    val_size = int((total_records - training_size) / 2)
+    return (
+        dataset.take(training_size),
+        dataset.skip(training_size).take(val_size),
+        dataset.skip(training_size).skip(val_size)
+    )
 
 
-def get_baseline_regressor(input_len: int, features: int) -> tf.keras.models.Model:
+def get_baseline_regressor() -> tf.keras.models.Model:
     """ Build a baseline network. """
     # Assemble the model:
     model = tf.keras.models.Sequential(
         name='Baseline-NN',
         layers=[
 
-            tf.keras.layers.Flatten(input_shape=[input_len, features]),
-            tf.keras.layers.Dense(input_len),
-            tf.keras.layers.Dense(1)
+            tf.keras.layers.Flatten(input_shape=[SEQ_LEN, FEATURES]),
+            tf.keras.layers.Dense(SEQ_LEN),
+            tf.keras.layers.Dense(TARGETS)
         ]
     )
 
@@ -107,41 +106,38 @@ def get_baseline_regressor(input_len: int, features: int) -> tf.keras.models.Mod
     return model
 
 
-def sample_random_seq(df: pd.DataFrame, time_steps: int = 3) -> tuple:
-    """ Sample a given dataframe and return a sequence of a specified length. """
-    # Prepare valid samples:
-    df = df.loc[list(df.index)[:-SEQ_LEN + time_steps]]
-    sample = np.random.choice(df.index)
-    x_samples = []
-    y_samples = []
-
-    for i in range(time_steps):
-        # Pick samples and apply format:
-        sample_index = list(range(sample + i, sample + SEQ_LEN + i))
-        results = fetch_preprocessed_data(df.loc[sample_index, ['Appliances', 'T_out', 'ampm']])
-        x_samples.append(np.reshape(results[['T_out', 'ampm']].values, (1, SEQ_LEN, FEATURES)))
-        y_samples.append(np.reshape(results[['Appliances']].values, (SEQ_LEN, 1)))
-
-    return x_samples, y_samples
+def train_model(model: tf.keras.models.Model, train_ds, val_ds) -> tf.keras.models.Model:
+    """ Pass a model and train it on a given dataset. """
+    # Start training:
+    version_name = get_model_version_name(model.name)
+    tb_logs = tf.keras.callbacks.TensorBoard(os.path.join(LOGS_DIR, version_name))
+    early_stop = tf.keras.callbacks.EarlyStopping(patience=PATIENCE, restore_best_weights=True)
+    lr_scheduler = tf.keras.callbacks.ReduceLROnPlateau(factor=.5, patience=int(PATIENCE/2))
+    model.fit(train_ds, validation_data=val_ds, epochs=EPOCHS, callbacks=[tb_logs, early_stop, lr_scheduler])
+    model.save(os.path.join(MODELS_DIR, f"{version_name}.h5"))
+    return model
 
 
 def dev():
     """ Test some shit. """
     # Train baseline model:
-    X_train, X_val = fetch_dataset(SEQ_LEN, BATCH_SIZE, TARGETS, FEATURES, 0.9)
-    model = get_baseline_regressor(SEQ_LEN, FEATURES)
-    model.fit(X_train, validation_data=X_val, epochs=10)
+    X_train, X_val, X_test = fetch_dataset()
+    # model = get_baseline_regressor()
+    # model = train_model(model, X_train, X_val)
+    model = tf.keras.models.load_model(r'models/Baseline-NN_v.20220411-194450.h5')
 
-    # Get samples to predict on:
-    df = fetch_raw_data(TARGETS)
-    x_samples, y_samples = sample_random_seq(df)
+    for x, y in X_test.take(1):
+        # Plot some predictions:
+        plt.plot(np.reshape(y[0], (-1)), 'b-')
+        plt.plot(model.predict(np.reshape(x[0], (1, SEQ_LEN, FEATURES)))[0], 'ro')
+
     return {}
 
 
 def main():
     """ Run script. """
-    X_train, X_val, scaler = fetch_dataset(SEQ_LEN, BATCH_SIZE, TARGETS, FEATURES, 0.9)
-    model = get_baseline_regressor(SEQ_LEN, FEATURES)
+    X_train, X_val, scaler = fetch_dataset(0.9)
+    model = get_baseline_regressor()
     model.fit(X_train, validation_data=X_val, epochs=10)
     return {}
 
